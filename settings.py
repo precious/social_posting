753542c83bbdced2ce834432c3e460dbca7fb29f
precious <precious@i.ua>
2012-09-21 15:58:46 +0300

    1st commit

---
+++ settings.py
@@ -0,0 +1,44 @@
+########## Twitter ##########
+# create application here https://dev.twitter.com/apps and set for it permissions: Read and Write
+twitter_tokens_dict = {
+	'oauth_consumer_key': '',
+	'oauth_token': '',
+}
+
+twitter_secrets_dict = {
+	'oauth_consumer_secret': '',
+	'oauth_token_secret': '',
+}
+
+########## Facebook ##########
+# 1. Creating app
+# create app here https://developers.facebook.com/apps and set for it
+# Settings > Basic > Sandbox Mode: Enabled
+# Settings > Persmissions > Extended Permissions: publish_stream, offline_access
+# Settings > Advanced > Remove offline_access > disabled
+# 2. Granting app permissions
+# go to https://graph.facebook.com/oauth/authorize?client_id=APP_ID&scope=publish_stream,offline_access&redirect_uri=http://www.facebook.com/connect/login_success.html
+# (dont forget to replace APP_ID with your own) and allow the required permissions. You should see page with 'Success' word.
+# Copy the 'code' paramater from browser's address bar
+# 3. Obtaining FB access_token for your app
+# # 1st way
+#   go to https://graph.facebook.com/oauth/access_token?client_id=APP_ID&redirect_uri=http://www.facebook.com/connect/login_success.html&client_secret=APP_SECRET&code=CODE
+#   (dont forget to replace APP_ID, APP_SECRET with your own values and CODE by previously obtained 'code' parameter value)
+#   open this url in your browser. You will see page with access_token.
+# # 2nd way 
+#   copy User Token from here: https://developers.facebook.com/tools/access_token/
+# Paste your access_token here https://developers.facebook.com/tools/debug and make sure
+# that is has expire date 'never'
+ 
+facebook_keys_dict = {
+	'app_id': '',
+	'app_secret': '',
+	# here should be access_token which never expires
+	'access_token': '',
+	'user_id': '',
+}
+
+try:
+	from settings_local import *
+except ImportError:
+	pass
---
+++ twitter.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+from settings import twitter_tokens_dict, twitter_secrets_dict
+import urllib, urllib2
+import random
+import string
+import time
+from hashlib import sha1
+import hmac
+import binascii
+
+
+Q = lambda x: urllib.quote(x,'')
+
+def get_hmac_sha1(key,raw_str):
+    hashed = hmac.new(key, raw_str, sha1)
+    return binascii.b2a_base64(hashed.digest())[:-1]
+
+
+def make_tweet(text):
+	base_url = 'https://api.twitter.com/1.1/statuses/update.json'
+	http_method = 'POST'
+
+	twitter_tokens_dict['oauth_nonce'] = ''.join(random.choice(string.ascii_uppercase + \
+		string.ascii_lowercase + string.digits) for i in range(42))
+	twitter_tokens_dict['oauth_timestamp'] = str(int(time.time()))
+	twitter_tokens_dict['oauth_signature_method'] = 'HMAC-SHA1'
+	twitter_tokens_dict['oauth_version'] = '1.0'
+	
+	# now its time to create signature
+	params_dict = {'status': text}
+	params_dict.update(twitter_tokens_dict)
+	params = [(Q(key), Q(params_dict[key])) for key in sorted(params_dict.keys())]
+	params_str = '&'.join(p[0] + '=' + p[1] for p in params) # urllib.urlencode isn't acceptable here
+	signature_base_string = http_method + '&' + Q(base_url) + '&' + Q(params_str)
+	signing_key = Q(twitter_secrets_dict['oauth_consumer_secret']) + '&' + \
+		Q(twitter_secrets_dict['oauth_token_secret'])
+	
+	# phew. seems like we have the signature now
+	twitter_tokens_dict['oauth_signature'] = get_hmac_sha1(signing_key,signature_base_string)
+	header_str = 'OAuth ' + ', '.join(
+		Q(key) + '="' + Q(twitter_tokens_dict[key]) + '"' for key in twitter_tokens_dict)
+
+	post_data = 'status=' + Q(text)
+
+	headers = {
+		'Authorization': header_str,
+		'Content-Length': str(len(post_data)),
+	}
+	
+	request = urllib2.Request(base_url,post_data,headers)
+	try:
+		response = urllib2.urlopen(request)
+	except:
+		return False
+	return True
